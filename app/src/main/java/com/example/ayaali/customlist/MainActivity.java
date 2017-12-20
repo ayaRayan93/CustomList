206eeb668b757137bd53eba2568d8da9a413ee89
ayaRayan93 <aya.rayan1011@gmail.com>
2017-12-20 13:52:44 +0200

    updateToolBar+addAboutUsPage

--- app/src/main/java/com/example/ayaali/customlist/MainActivity.java
+++ app/src/main/java/com/example/ayaali/customlist/MainActivity.java
@@ -1,17 +1,22 @@
 package com.example.ayaali.customlist;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener,NavigationView.OnNavigationItemSelectedListener{
 
     private final String[] listOfText={"facebook","twitter","googleplus","linkedin","wordpress","youtube"};
     private final int[] imagesIds={R.mipmap.facebook,R.mipmap.twitter,R.mipmap.googleplus,R.mipmap.linkedin,R.mipmap.wordpress,R.mipmap.youtube};
@@ -26,7 +31,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
         listView.setOnItemClickListener(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -39,5 +45,30 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         Toast.makeText(MainActivity.this,"clivked on"+listOfText[position],Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.aboutUs) {
+            Intent main=new Intent("AboutUs");
+            startActivity(main);
+
+        } else if (id == R.id.contactUs) {
+
+            Intent main=new Intent("MainActivity");
+            startActivity(main);
+
+        } else if (id == R.id.webPage) {
+
+            Uri uriUrl = Uri.parse("http://hadeya.net/");
+            Intent launchBrowser = new Intent(Intent.ACTION_VIEW, uriUrl);
+            startActivity(launchBrowser);
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
