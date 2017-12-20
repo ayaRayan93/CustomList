9cd0d51d2fa7a36075b5ced9759bffe89a8a327e
ayaRayan93 <aya.rayan1011@gmail.com>
2017-12-21 00:15:47 +0200

    updateToolBar+addAboutUsPage

---
+++ app/src/main/java/com/example/ayaali/customlist/StarterActivity.java
@@ -0,0 +1,34 @@
+package com.example.ayaali.customlist;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StarterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starter);
+        Thread timer=new Thread() {
+            public void run() {
+
+                try
+                {
+                    sleep(3000);
+                }
+                catch (InterruptedException e)
+                {
+                    e.printStackTrace();
+                }
+                finally {
+                    Intent main=new Intent("MainActivity");
+                    startActivity(main);
+                    finish();
+                }
+            }
+
+        };
+        timer.start();
+    }
+}
